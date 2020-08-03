ea7f1a7cfaad1af23d2f15a8b348eebb891bb528
bakti <bakti.widianto@gmail.com>
2020-08-03 21:35:42 +0700

    connect to firebase

--- server/app.js
+++ server/app.js
@@ -2,6 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const firebase = require('firebase');
+
+const config = {
+    apiKey: "AIzaSyCFagSBEqolX8ymq5n4RQ46HAQGGolGXWc",
+    authDomain: "phonebook-6e223.firebaseapp.com",
+    databaseURL: "https://phonebook-6e223.firebaseio.com",
+    projectId: "phonebook-6e223",
+    storageBucket: "phonebook-6e223.appspot.com",
+    messagingSenderId: "29565688711"
+};
+firebase.initializeApp(config);
+
+
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,62 @@
 var express = require('express');
 var router = express.Router();
+const firebase = require("firebase");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* ROUTER FOR TESTING PURPOSE ONLY */
+
+router.get('/', function (req, res, next) {
+  const phoneReference = firebase.database().ref("/Phones/");
+  phoneReference.on("value", function (snapshot) {
+    if (!snapshot.val()) {
+      console.log('Not yet data in database')
+    }
+    res.json(snapshot.val());
+    phoneReference.off("value");
+  }, function (errorObject) {
+    console.log("Read data failed" + errorObject.code);
+    res.send("Read data failed" + errorObject.code)
+  });
 });
+
+router.post('/', function (req, res) {
+  const id = new Date().getTime();
+  const { name, phone } = req.body;
+  const referencePath = `/Phones/${id}/`;
+  const phoneReference = firebase.database().ref(referencePath);
+  phoneReference.set({ name: name, phone: phone }, function (error) {
+    if (error) {
+      res.send("Data could not be saved." + error)
+    } else {
+      res.send("Data saved successfully.");
+    }
+  });
+});
+
+router.put('/:id', function (req, res) {
+  const { name, phone } = req.body;
+  const id = req.params.id;
+  const referencePath = `/Phones/${id}/`;
+  const phoneReference = firebase.database().ref(referencePath);
+  phoneReference.update({ name, phone }, function (error) {
+    if (error) {
+      res.send("Data cloud not be updated" + error)
+    } else {
+      res.send("Data updated successfully");
+    }
+  })
+})
+
+router.delete('/:id', function (req, res) {
+  const id = req.params.id
+  const referencePath = `/Phones/${id}/`;
+  const phoneReference = firebase.database().ref(referencePath);
+  phoneReference.remove(error => {
+    if (error) {
+      res.send('Data could not be deleted.' + error);
+    } else {
+      res.send('Data deleted successfully.');
+    }
+  })
+})
 
 module.exports = router;
