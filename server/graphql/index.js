9610f0e3ee915942a26e642a4b45b62051676c0e
bakti <bakti.widianto@gmail.com>
2020-08-04 04:52:48 +0700

    modifed

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -1,6 +1,12 @@
 var GraphQlSchema = require('graphql').GraphQLSchema;
+var GraphQLObjectType = require('graphql').GraphQLObjectType;
 var queryType = require('./queries/phone').queryType;
+const mutation = require('./mutations/index');
 
 exports.phoneSchema = new GraphQlSchema({
-    query: queryType
+    query: queryType,
+    mutation: new GraphQLObjectType({
+        name: 'Mutation',
+        fields: mutation
+    })
 })
