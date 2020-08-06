cccd6e342617597d992edb7db28638b4e98e372a
bakti-widianto <bakti.widianto@gmail.com>
2020-08-07 01:36:17 +0700

    post database

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import ApoloClient from 'apollo-boost';
 import gql from 'graphql-tag'
-
+import Swal from 'sweetalert2';
 
 const API_URL = 'http://localhost:3001/graphql';
 const client = new ApoloClient({
@@ -44,9 +44,17 @@ export const loadContacts = () => {
 //end load data
 
 //start post data
+const postContactSuccess = (contact) => ({
+    type: 'POST_CONTACT_SUCCESS',
+    contact
+})
 
+const postContactFailure = (id) => ({
+    type: 'POST_CONTACT_FAILURE',
+    id,
+})
 
-export const postContactRedux = (id, name, phone) => ({
+const postContactRedux = (id, name, phone) => ({
     type: 'POST_CONTACT',
     id,
     name,
@@ -56,11 +64,41 @@ export const postContactRedux = (id, name, phone) => ({
 export const postContact = (name, phone) => {
     console.log(name, phone)
     const id = new Date().getTime();
-
-
-
+    const addQuery = gql`
+    mutation addContact($id: ID!, $name: String!, $phone: String!) {
+        addContact(id: $id, name: $name, phone: $phone) {
+            id
+            name
+            phone
+        }
+    }`;
 
     return dispath => {
         dispath(postContactRedux(id, name, phone));
+
+        return client.mutate({
+            mutation: addQuery,
+            variables: {
+                id,
+                name,
+                phone
+            }
+        })
+            .then(function (response) {
+                Swal.fire(
+                    'Success!',
+                    'Contact added successfully!',
+                    'success'
+                )
+                dispath(postContactSuccess(response.data.addContact));
+            })
+            .catch(function (error) {
+                Swal.fire(
+                    'Error!',
+                    'connection problem try again later',
+                    'error'
+                )
+                dispath(postContactFailure(id));
+            })
     }
 }
--- client/src/reducers/contacts.js
+++ client/src/reducers/contacts.js
@@ -20,6 +20,7 @@ const contacts = (state = initState, action) => {
                     return item;
                 }),
             }
+
         case 'POST_CONTACT':
             return {
                 ...state,
@@ -33,6 +34,28 @@ const contacts = (state = initState, action) => {
                     }
                 ]
             }
+
+        case 'POST_CONTACT_SUCCESS':
+            return {
+                ...state,
+                contacts: state.contacts.map(item => {
+                    item.sent = true;
+                    return item
+                })
+            }
+
+        case 'POST_CONTACT_FAILURE':
+            return {
+                ...state,
+                contacts: state.contacts.map(item => {
+                    if (item.id === action.id) {
+                        item.sent = false;
+                    }
+                    return item
+                })
+            }
+
+
 
         case 'LOAD_CONTACT_FAILURE':
         default:
